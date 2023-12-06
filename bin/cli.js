b55d86bda35f2a30157e46bca8736e15899e2c1e
Pavel Popov <pavel@findhotel.net>
2023-12-06 23:13:32 +0100

    Format code with prettier

--- bin/cli.js
+++ bin/cli.js
@@ -1,25 +1,25 @@
 #!/usr/bin/env node
 
-import fs from 'fs';
+import fs from "fs";
 import stringify from "json-stringify-pretty-compact";
 import commandLineArgs from "command-line-args";
 
 const opts = commandLineArgs([
-  { name: 'start', type: Number, default: 0 },
-  { name: 'end', type: Number },
-  { name: 'indent', type: Number, default: 2 },
-  { name: 'maxlength', type: Number, default: 120 },
-])
+  { name: "start", type: Number, default: 0 },
+  { name: "end", type: Number },
+  { name: "indent", type: Number, default: 2 },
+  { name: "maxlength", type: Number, default: 120 },
+]);
 
 // Read file
-const original = fs.readFileSync(0, 'utf-8');
+const original = fs.readFileSync(0, "utf-8");
 let json;
-let text
+let text;
 try {
   if (opts.start && opts.end) {
-    text = original.substring(opts.start, opts.end)
+    text = original.substring(opts.start, opts.end);
   } else {
-    text = original
+    text = original;
   }
   json = JSON.parse(text);
 } catch (e) {
@@ -28,14 +28,17 @@ try {
 }
 
 // Format
-const output = stringify(json, {maxLength: opts.maxlength, indent: opts.indent});
+const output = stringify(json, {
+  maxLength: opts.maxlength,
+  indent: opts.indent,
+});
 
 // Write output
 if (opts.start && opts.end) {
-  process.stdout.write(original.substring(0, opts.start))
-  process.stdout.write(output)
-  process.stdout.write(original.substring(opts.end))
+  process.stdout.write(original.substring(0, opts.start));
+  process.stdout.write(output);
+  process.stdout.write(original.substring(opts.end));
 } else {
-  process.stdout.write(output)
+  process.stdout.write(output);
 }
-process.stdout.write('\n')
+process.stdout.write("\n");
